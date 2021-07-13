30b38137b7d3d308427492946f02cf41edd6918e
Adam Kirosingh <akirosingh@nevada.unr.edu>
2021-07-13 16:36:42 -0700

    Uncommenting

--- R/zip_datafiles.R
+++ R/zip_datafiles.R
@@ -15,7 +15,7 @@
 zip_datafiles <- function(genofile, gmapfile, pmapfile, foundergenofile,
                           phenofile, controlfile) {
 
-zip(zipfile = here("data/prepared_data","do_malaria"), files = c(here("data/prepared_data/covar.csv"),here("data/prepared_data/foundergeno.csv"),here("data/prepared_data/geno.csv"),here("data/prepared_data/gmap.csv"),here("data/prepared_data/pheno.csv"),here("data/prepared_data/pmap.csv"),here("data/prepared_data/controlfile.json")))
+zip(zipfile = here("data/prepared_data","do_malaria.zip"), files = c(here("data/prepared_data/covar.csv"),here("data/prepared_data/foundergeno.csv"),here("data/prepared_data/geno.csv"),here("data/prepared_data/gmap.csv"),here("data/prepared_data/pheno.csv"),here("data/prepared_data/pmap.csv"),here("data/prepared_data/controlfile.json")))
 
   here("data/prepared_data","do_malaria.zip")
 }
